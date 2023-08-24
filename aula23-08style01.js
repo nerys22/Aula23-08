3c3b65f99b49ca9445775da71d4e6d7a284bd0ef
nerys22 <142638485+nerys22@users.noreply.github.com>
2023-08-23 21:35:10 -0300

    Create aula23-08style01.js

---
+++ aula23-08style01.js
@@ -0,0 +1,6 @@
+document.write(" <br> Email: fnmartins1@gmail.com <br>");
+document.write("Facil aprendizagem <br>");
+document.write("Pacote office <br>");
+document.write("Tecnico de projetores <br>");
+document.write("Tecnico de redes");
+document.querySelector("h1").innerHTML="Curriculo";
